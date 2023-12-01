03a9bf6e016e7c9e75f59c0cd8230ff167f262c6
celerywastaken <152607041+celerywastaken@users.noreply.github.com>
2023-12-01 12:55:16 +0100

    Initial Commit
    Added Kitchen Asset
    - fox asset and sounds
    -atmosphere sounds

---
+++ Assets/FoundationsOfAudio/Scripts/PlayAudio.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            music.Play();
+        }
+        
+    }
+}
